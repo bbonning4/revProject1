3d706c5330c578df4b727d357cb29cc8034d322d
Bo <bbonning42@gmail.com>
2024-08-01 14:38:48 -0500

    user getter/setters

--- Project1/src/main/java/com/revature/Project1/models/User.java
+++ Project1/src/main/java/com/revature/Project1/models/User.java
@@ -43,35 +43,35 @@ public class User {
                 '}';
     }
 
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public List<Cat> getCats() {
-//        return cats;
-//    }
-//
-//    public void setCats(List<Cat> cats) {
-//        this.cats = cats;
-//    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Cat> getCats() {
+        return cats;
+    }
+
+    public void setCats(List<Cat> cats) {
+        this.cats = cats;
+    }
 }
