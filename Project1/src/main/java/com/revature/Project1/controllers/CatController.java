72a62581a0821125f5a4aae651820bcd4ea04e4f
eganokumugit <99830072+eganokumugit@users.noreply.github.com>
2024-08-02 14:04:44 -0400

    Cat stuff

---
+++ Project1/src/main/java/com/revature/Project1/controllers/CatController.java
@@ -0,0 +1,51 @@
+package com.revature.Project1.controllers;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.Project1.models.Cat;
+import com.revature.Project1.services.CatService;
+import lombok.experimental.PackagePrivate;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cats")
+@CrossOrigin
+public class CatController {
+
+    CatService cs;
+    ObjectMapper mp;
+    @Autowired //Constructor Injection
+    public CatController(CatService cs, ObjectMapper mp) {
+        this.cs = cs;
+        this.mp = mp;
+    }
+
+    @GetMapping("/cats")
+    public ResponseEntity<String> getAllCats() throws JsonProcessingException {
+        List<Cat> catList = cs.getAllCats();
+        String jsonResponse = mp.writeValueAsString(catList);
+        return ResponseEntity.status(200).body(jsonResponse);
+    }
+
+    @GetMapping("cats/{id}")
+    public ResponseEntity<String> getCat(@PathVariable int id) throws JsonProcessingException {
+        Cat cat = cs.getCat(id);
+        String jsonResponse = cat != null ? mp.writeValueAsString(cat) : "";
+        return ResponseEntity.status(200).body(jsonResponse);
+    }
+
+
+
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Cat> addCat(@RequestBody Cat a) {
+        a = cs.addCat(a);
+        return new ResponseEntity<>(a, HttpStatus.OK);
+    }
+
+}
+
